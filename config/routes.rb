48c5eca823dc82ec9f52c13d29c67d92b4075b1b
Peter Donald <peter@realityforge.org>
2006-04-16 11:44:49 +0000

    * Make default admin option listing galleries * Add in directory that images can be cached in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'account/:action', :controller => 'account'
+  map.connect 'admin', :controller => 'album', :action => 'list'
   map.connect 'admin/album/:action/:id', :controller => 'album'
   map.connect 'admin/picture/:action/:id', :controller => 'picture'
-  map.connect ':user/:album/:size/:picture', :controller => 'gallery', :action => 'show_image', :requirements => {:picture => /\d+.\w+/, :size => /thumbnail|large|original/}
+  map.connect ':user/:album/images/:size/:picture', :controller => 'gallery', :action => 'show_image', :requirements => {:picture => /\d+.\w+/, :size => /thumbnail|large|original/}
   map.connect ':user/:album/:picture', :controller => 'gallery', :action => 'show', :picture => '1'
   map.connect ':user', :controller => 'gallery', :action => 'list'
   map.connect '', :controller => 'gallery', :action => 'user_list'
