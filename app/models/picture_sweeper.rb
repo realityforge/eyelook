bfced044d5a73792ee06f8b1024cd24d427586e2
Peter Donald <peter@realityforge.org>
2006-04-23 09:27:33 +0000

    Do not remove cached images on update - just on destroy

--- app/models/picture_sweeper.rb
+++ app/models/picture_sweeper.rb
@@ -1,14 +1,12 @@
 class PictureSweeper < ActionController::Caching::Sweeper
   observe Picture
 
-  def after_save(picture)
-    ['thumbnail','large','original'].each do |size|
+  def after_destroy(picture)
+    ::ImageGeometry.keys.each do |size|
       expire_page(:controller => 'gallery', 
                   :action => 'show_image', 
                   :size => size, 
                   :image => picture.filename)
     end
   end
-
-  alias :after_destroy :after_save
 end
